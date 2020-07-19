62f8c9e1e17661e32553a6da210d485aea6861fb
Kevin Jiang <kevin51jiang@email.com>
2020-07-19 07:43:25 -0400

    finished code

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 var JsonDB = require("node-json-db").JsonDB;
 var Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
-const fuse = require("fuse.js");
-const utils = require('./utils');
+const utils = require("./utils");
 
 const db = new JsonDB(new Config("todoDB", true, true, "/"));
 
@@ -20,12 +19,10 @@ if (!slackSigningSecret || !slackAccessToken) {
     "A Slack signing secret and access token are required to run this app."
   );
 }
-///////////////////////////// 
+/////////////////////////////
 // SELF DEFINED CONSTANTS //
 ////////////////////////////
 const categories = ["todo", "finished", "blocker"];
-
-
 
 const slackInteractions = createMessageAdapter(slackSigningSecret);
 const web = new WebClient(slackAccessToken);
@@ -39,7 +36,9 @@ app.use("/slack/actions", slackInteractions.expressMiddleware());
 // Attach the slash command handler
 app.post(
   "/slack/commands",
-  bodyParser.urlencoded({ extended: false }),
+  bodyParser.urlencoded({
+    extended: false,
+  }),
   slackSlashCommand
 );
 
@@ -49,209 +48,188 @@ http.createServer(app).listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
 
-// Slack interactive message handlersd
-slackInteractions.action("accept_tos", (payload, respond) => {
-  console.log(
-    `The user ${payload.user.name} in team ${payload.team.domain} pressed a button`
-  );
+// slackInteractions
+//   .options(
+//     { callbackId: "pick_sf_neighborhood", within: "interactive_message" },
+//     (payload) => {
+//       console.log(
+//         `The user ${payload.user.name} in team ${payload.team.domain} has requested options`
+//       );
 
-  // Use the data model to persist the action
-  users
-    .findBySlackId(payload.user.id)
-    .then((user) =>
-      user.setPolicyAgreementAndSave(payload.actions[0].value === "accept")
-    )
-    .then((user) => {
-      // After the asynchronous work is done, call `respond()` with a message object to update the
-      // message.
-      let confirmation;
-      if (user.agreedToPolicy) {
-        confirmation = "Thank you for agreeing to the terms of service";
-      } else {
-        confirmation =
-          "You have denied the terms of service. You will no longer have access to this app.";
-      }
-      respond({ text: confirmation });
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error(error);
-      respond({
-        text: "An error occurred while recording your agreement choice.",
+//       // Gather possible completions using the user's input
+//       return (
+//         neighborhoods
+//           .fuzzyFind(payload.value)
+//           // Format the data as a list of options
+//           .then(formatNeighborhoodsAsOptions)
+//           .catch((error) => {
+//             console.error(error);
+//             return { options: [] };
+//           })
+//       );
+//     }
+//   )
+//   .action("pick_sf_neighborhood", (payload, respond) => {
+//     console.log(
+//       `The user ${payload.user.name} in team ${payload.team.domain} selected from a menu`
+//     );
+
+//     // Use the data model to persist the action
+//     neighborhoods
+//       .find(payload.actions[0].selected_options[0].value)
+//       // After the asynchronous work is done, call `respond()` with a message object to update the
+//       // message.
+//       .then((neighborhood) => {
+//         respond({
+//           text: payload.original_message.text,
+//           attachments: [
+//             {
+//               title: neighborhood.name,
+//               title_link: neighborhood.link,
+//               text: "One of the most interesting neighborhoods in the city.",
+//             },
+//           ],
+//         });
+//       })
+//       .catch((error) => {
+//         // Handle errors
+//         console.error(error);
+//         respond({
+//           text: "An error occurred while finding the neighborhood.",
+//         });
+//       });
+
+//     // Before the work completes, return a message object that is the same as the original but with
+//     // the interactive elements removed.
+//     const reply = payload.original_message;
+//     delete reply.attachments[0].actions;
+//     return reply;
+//   });
+
+// const pick_sf_neighborhood = {
+//   text: "San Francisco is a diverse city with many different neighborhoods.",
+//   response_type: "in_channel",
+//   attachments: [
+//     {
+//       text: "Explore San Francisco",
+//       callback_id: "pick_sf_neighborhood",
+//       actions: [
+//         {
+//           name: "neighborhood",
+//           text: "Choose a neighborhood",
+//           type: "select",
+//           data_source: "external",
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// Example of handling static select (a type of block action)
+slackInteractions.action(
+  {
+    type: "static_select",
+  },
+  (payload, respond) => {
+    try {
+      const userId = payload.user.id;
+      const selectedOpt = payload.actions[0].selected_option.value;
+
+      let numberOfElement = db.count(`/${userId}/todo`);
+      console.log("numberOfElement", numberOfElement);
+
+      const status = [...Array(numberOfElement)].forEach((_, i) => {
+        const todoAt = db.getData(`/${userId}/todo[${[i]}]`);
+        console.log("comparing ", todoAt, selectedOpt);
+        if (todoAt === selectedOpt) {
+          //create first, then delete
+          db.push(`/${userId}/finished[]`, selectedOpt, true);
+          db.delete(`/${userId}/todo[${i}]`); // delete second
+          respond({
+            text: `_"${selectedOpt}"_ has been moved to the finished list.`,
+          });
+        }
       });
-    });
+    } catch (err) {
+      respond({
+        text: `Sorry, that action could not be done. Please try again later. Err: ${err}`,
+      });
+    }
+  }
+);
 
-  // Before the work completes, return a message object that is the same as the original but with
-  // the interactive elements removed.
-  const reply = payload.original_message;
-  delete reply.attachments[0].actions;
-  return reply;
+/**
+ * Cancel button
+ */
+slackInteractions.action({ action_id: "cancel_remove" }, (payload, respond) => {
+  respond({ text: "Cancelled." });
 });
 
-slackInteractions
-  .options(
-    { callbackId: "pick_sf_neighborhood", within: "interactive_message" },
-    (payload) => {
-      console.log(
-        `The user ${payload.user.name} in team ${payload.team.domain} has requested options`
-      );
-
-      // Gather possible completions using the user's input
-      return (
-        neighborhoods
-          .fuzzyFind(payload.value)
-          // Format the data as a list of options
-          .then(formatNeighborhoodsAsOptions)
-          .catch((error) => {
-            console.error(error);
-            return { options: [] };
-          })
-      );
-    }
-  )
-  .action("pick_sf_neighborhood", (payload, respond) => {
-    console.log(
-      `The user ${payload.user.name} in team ${payload.team.domain} selected from a menu`
-    );
-
-    // Use the data model to persist the action
-    neighborhoods
-      .find(payload.actions[0].selected_options[0].value)
-      // After the asynchronous work is done, call `respond()` with a message object to update the
-      // message.
-      .then((neighborhood) => {
-        respond({
-          text: payload.original_message.text,
-          attachments: [
+const pickTaskToFinish = {
+  attachments: [
+    {
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Pick a finished item from the dropdown list",
+          },
+          accessory: {
+            type: "static_select",
+            placeholder: {
+              type: "plain_text",
+              text: "Select an todo",
+              emoji: true,
+            },
+            action_id: "submit-del-choice",
+            confirm: {
+              title: {
+                type: "plain_text",
+                text: "Mark as finished",
+              },
+              text: {
+                type: "plain_text",
+                text: "Are you sure you want to mark this item as finished?",
+              },
+              confirm: {
+                type: "plain_text",
+                text: "Yes",
+              },
+              deny: {
+                type: "plain_text",
+                text: "No",
+              },
+            },
+            // OPTIONS HERE
+            options: [
+              {
+                text: {
+                  type: "plain_text",
+                  text: "*this is plain_text text*",
+                  emoji: true,
+                },
+                value: "value-0",
+              },
+            ],
+          },
+        },
+        {
+          type: "actions",
+          elements: [
             {
-              title: neighborhood.name,
-              title_link: neighborhood.link,
-              text: "One of the most interesting neighborhoods in the city.",
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Cancel",
+                emoji: true,
+              },
+              action_id: "cancel-remove",
+              style: "danger",
             },
           ],
-        });
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error(error);
-        respond({
-          text: "An error occurred while finding the neighborhood.",
-        });
-      });
-
-    // Before the work completes, return a message object that is the same as the original but with
-    // the interactive elements removed.
-    const reply = payload.original_message;
-    delete reply.attachments[0].actions;
-    return reply;
-  });
-
-slackInteractions.action({ type: "dialog_submission" }, (payload, respond) => {
-  // `payload` is an object that describes the interaction
-  console.log(
-    `The user ${payload.user.name} in team ${payload.team.domain} submitted a dialog`
-  );
-
-  // Check the values in `payload.submission` and report any possible errors
-  const errors = validateKudosSubmission(payload.submission);
-  if (errors) {
-    return errors;
-  } else {
-    setTimeout(() => {
-      const partialMessage = `<@${payload.user.id}> just gave kudos to <@${payload.submission.user}>.`;
-
-      // When there are no errors, after this function returns, send an acknowledgement to the user
-      respond({
-        text: partialMessage,
-      });
-
-      // The app does some work using information in the submission
-      users
-        .findBySlackId(payload.submission.id)
-        .then((user) => user.incrementKudosAndSave(payload.submission.comment))
-        .then((user) => {
-          // After the asynchronous work is done, call `respond()` with a message object to update
-          // the message.
-          respond({
-            text: `${partialMessage} That makes a total of ${user.kudosCount}! :balloon:`,
-            replace_original: true,
-          });
-        })
-        .catch((error) => {
-          // Handle errors
-          console.error(error);
-          respond({ text: "An error occurred while incrementing kudos." });
-        });
-    });
-  }
-});
-
-// Example interactive messages
-const interactiveButtons = {
-  text:
-    "The terms of service for this app are _not really_ here: <https://unsplash.com/photos/bmmcfZqSjBU>",
-  response_type: "in_channel",
-  attachments: [
-    {
-      text: "Do you accept the terms of service?",
-      callback_id: "accept_tos",
-      actions: [
-        {
-          name: "accept_tos",
-          text: "Yes",
-          value: "accept",
-          type: "button",
-          style: "primary",
-        },
-        {
-          name: "accept_tos",
-          text: "No",
-          value: "deny",
-          type: "button",
-          style: "danger",
         },
       ],
-    },
-  ],
-};
-
-const pick_sf_neighborhood = {
-  text: "San Francisco is a diverse city with many different neighborhoods.",
-  response_type: "in_channel",
-  attachments: [
-    {
-      text: "Explore San Francisco",
-      callback_id: "pick_sf_neighborhood",
-      actions: [
-        {
-          name: "neighborhood",
-          text: "Choose a neighborhood",
-          type: "select",
-          data_source: "external",
-        },
-      ],
-    },
-  ],
-};
-
-const dialog = {
-  callback_id: "kudos_submit",
-  title: "Give kudos",
-  submit_label: "Give",
-  elements: [
-    {
-      label: "Teammate",
-      type: "select",
-      name: "user",
-      data_source: "users",
-      placeholder: "Teammate Name",
-    },
-    {
-      label: "Comment",
-      type: "text",
-      name: "comment",
-      placeholder: "Thanks for helping me with my project!",
-      hint: "Describe why you think your teammate deserves kudos.",
     },
   ],
 };
@@ -267,15 +245,23 @@ function slackSlashCommand(req, res, next) {
       res.json(response);
     } else if (type === "finish" || type === "fin" || type === "done") {
       // END NEW TODO
-      const response = finishTodo(req.body);
+      const todoOpts = getTodoOpts(req.body.user_id);
+      const response = {
+        ...pickTaskToFinish,
+      };
+      response.attachments[0].blocks[0].accessory.options = todoOpts.fields;
+
       res.json(response);
     } else if (type === "standup") {
       // SEND STANDUP
       const response = standup(req.body);
       res.json(response);
+      //clean up finished and blockers
+      db.delete(`/${req.body.user_id}/finished`);
+      db.delete(`/${req.body.user_id}/blocker`);
     } else if (type === "list" || type === "display") {
       // LIST CURRENT TASKS
-      const response = display(req.body.user_id)
+      const response = display(req.body.user_id);
       res.json(response);
     } else if (type === "blocker") {
       // ADD BLOCKER
@@ -288,7 +274,7 @@ function slackSlashCommand(req, res, next) {
     } else {
       // Help
       res.send(
-        "Use this command followed by `add <string>`, `finish <string>`, or `standup`."
+        "Use this command followed by `add <string>`, `blocker`, or `finish` to move tasks around. Use `list` or `standup` to view the current status. Note: `standup` removes finished tasks."
       );
     }
   } else {
@@ -326,12 +312,12 @@ const standup = (body) => {
     const userId = body.user_id;
     const now = new Date();
     const title = {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `*<@${userId}>'s standup for ${now.toDateString()}*`
-      }
-    }
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*<@${userId}>'s standup for ${now.toDateString()}*`,
+      },
+    };
 
     const standupResults = display(userId);
     standupResults[0] = title; // update title for daily standup
@@ -353,10 +339,10 @@ const getBlockFrom = (userId, attrib) => {
     type: "section",
     fields: [...Array(numberOfElement)].map((_, i) => {
       return {
-        "type": 'mrkdwn',
-        text: db.getData(`/${userId}/${attrib}[${i}]`)
-      }
-    })
+        type: "mrkdwn",
+        text: db.getData(`/${userId}/${attrib}[${i}]`),
+      };
+    }),
   };
   return returnable;
 };
@@ -370,12 +356,12 @@ const display = (userId) => {
 
     let blocks = [];
     const title = {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `*<@${userId}>'s current status for ${now.toDateString()}*`
-      }
-    }
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*<@${userId}>'s current status for ${now.toDateString()}*`,
+      },
+    };
 
     blocks.push(title);
 
@@ -387,15 +373,26 @@ const display = (userId) => {
           text: `*${utils.capitalizeString(category)}*`,
         },
       };
-      const catBlock = getBlockFrom(userId, category)
+      let catBlock;
+      try {
+        catBlock = getBlockFrom(userId, category);
 
-      if (category === 'finished') {
-        catBlock.fields = catBlock.fields.map((field) => {
-          return {
-            ...field,
-            text: `~${field.text}~`
-          }
-        })
+        if (category === "finished") {
+          catBlock.fields = catBlock.fields.map((field) => {
+            return {
+              ...field,
+              text: `~${field.text}~`,
+            };
+          });
+        }
+      } catch (err) {
+        catBlock = {
+          type: "section",
+          fields: [{
+            type: "plain_text",
+            text: "Empty!",
+          }],
+        };
       }
 
       acc.push(catTitle, catBlock, divider);
@@ -404,25 +401,13 @@ const display = (userId) => {
 
     let displayResults = {
       response_type: "in_channel",
-      blocks: blocks
+      blocks: blocks,
     };
 
     return displayResults;
-
   } catch (err) {
     return {
       text: `<@${userId}>, your standup has failed due to ${err}. Please try again.`,
-    };
-  }
-};
-
-const finishTodo = (body) => {
-  try {
-    const userId = body.user_id
-    db.getIndex(`/${userId}/todo/`)
-  } catch (err) {
-    return {
-      text: `<@${body.user_id}>, your standup has failed due to ${err}. Please try again.`,
     };
   }
 };
@@ -443,14 +428,22 @@ const clearTodos = (userId) => {
 };
 
 const getTodoOpts = (userId) => {
-  const unfinished = db.getData(`${userId}/todos`);
+  let numberOfElement = db.count(`/${userId}/todo`);
 
-  return unfinished.map((todo) => {
-    return {
-      text: todo,
-      value: todo,
-    };
-  });
+  const returnable = {
+    fields: [...Array(numberOfElement)].map((_, i) => {
+      const todo = db.getData(`/${userId}/todo[${i}]`);
+      return {
+        text: {
+          type: "plain_text",
+          text: todo,
+          emoji: true,
+        },
+        value: todo,
+      };
+    }),
+  };
+  return returnable;
 };
 
 // Helpers
@@ -463,6 +456,8 @@ function validateKudosSubmission(submission) {
     });
   }
   if (errors.length > 0) {
-    return { errors };
+    return {
+      errors,
+    };
   }
 }
